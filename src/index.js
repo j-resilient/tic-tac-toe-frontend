b1b37cd24e5013273ec92b3285ca27c3714f964f
J Resilient <j.resilient.1@gmail.com>
2021-07-26 17:02:17 -0400

    Create new Game and View in index.js to start the game and generate the board

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ const View = require("./ttt-view");
 const Game = require("../back_end/game");
 
   $(() => {
-    // Your code here 
+    const game = new Game();
+    const figure = $("figure.ttt");
+    const view = new View(game, figure);
   });
