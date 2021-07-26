5b58d50152b8dc0192985b6ba7a758a860135733
J Resilient <j.resilient.1@gmail.com>
2021-07-26 17:01:35 -0400

    Add code to ttt-view.js to create and add the tic-tac-toe board

--- src/ttt-view.js
+++ src/ttt-view.js
@@ -1,11 +1,24 @@
 class View {
-  constructor(game, $el) {}
+  constructor(game, $el) {
+    this.setupBoard($el);
+  }
 
   bindEvents() {}
 
   makeMove($square) {}
 
-  setupBoard() {}
+  setupBoard($el) {
+    const $ul = $("<ul></ul");
+    $ul.addClass("grid")
+
+    for (let i = 0; i < 9; i++) {
+      let $li = $("<li></li>");
+      $li.addClass("cell unclicked-cell");
+      $ul.append($li);
+    }
+
+    $el.append($ul)
+  }
 }
 
 module.exports = View;
