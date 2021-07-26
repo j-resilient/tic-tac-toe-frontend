9f8fa32960867f0dfbc38429eb822b33f8da0491
J Resilient <j.resilient.1@gmail.com>
2021-07-26 17:58:50 -0400

    Add code to ttt-view.js to play the game

--- src/ttt-view.js
+++ src/ttt-view.js
@@ -1,23 +1,50 @@
 class View {
   constructor(game, $el) {
+    this.game = game;
     this.setupBoard($el);
   }
 
-  bindEvents() {}
+  bindEvents() {
+    $("ul").on("click", "li", e => {
+      let $square = $(e.currentTarget);
 
-  makeMove($square) {}
+      this.makeMove($square);
+
+      $square.removeClass("unclicked-cell");
+      $square.addClass("clicked-cell");
+
+      // if game won, alert win
+      if (this.game.isOver()) {
+        alert(this.game.winner() + " wins!");
+      }
+    });
+  }
+
+  makeMove($square) {
+    let mark = this.game.currentPlayer;
+    try {
+      this.game.playMove($square.data("pos"));
+    } catch(err) {
+      alert(err.msg);
+      return;
+    }
+    $square.append(mark);
+  }
 
   setupBoard($el) {
     const $ul = $("<ul></ul");
     $ul.addClass("grid")
 
-    for (let i = 0; i < 9; i++) {
-      let $li = $("<li></li>");
-      $li.addClass("cell unclicked-cell");
-      $ul.append($li);
+    for (let x = 0; x < 3; x++) {
+      for (let y = 0; y < 3; y++) {
+        let $li = $("<li></li>");
+        $li.addClass("cell unclicked-cell");
+        $li.data("pos", [x, y]);
+        $ul.append($li);
+      }
     }
-
     $el.append($ul)
+    this.bindEvents();
   }
 }
 
